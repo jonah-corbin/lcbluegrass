c835a8ec95e23913876da1edea1b587dc36afc96
Jackson Lawrence <jlawrence1787@gmail.com>
2024-06-11 12:20:27 -0400

    sample deployment

--- api/index.js
+++ api/index.js
@@ -1,15 +1,8 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 
-app.set("view engine", "ejs");
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
